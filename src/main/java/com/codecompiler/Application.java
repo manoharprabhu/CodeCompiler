b96d2244607ddcb367cccd26a19ded26583a5dca
manoharprabhu <manoharagent13@gmail.com>
2016-05-30 13:59:07 +0530

    Added configuration parser and mongodb config

--- src/main/java/com/codecompiler/Application.java
+++ src/main/java/com/codecompiler/Application.java
@@ -1,16 +1,44 @@
 package com.codecompiler;
 
+import com.codecompiler.configuration.MongoConfiguration;
+import org.apache.commons.cli.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
 
 /**
  * Created by Manohar Prabhu on 5/27/2016.
  */
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+    private static Logger logger = Logger.getLogger(Application.class.toString());
+
     public static void main(String[] args) {
+        Options options = new Options();
+        options.addOption("mongohost", "Hostname of MongoDB server");
+        options.addOption("mongodatabase", "Database to use");
+
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine command = null;
+        try {
+            command = commandLineParser.parse(options, args);
+        } catch(ParseException e) {
+            logger.info("Invalid option passed.");
+            return;
+        }
+
+        if(command.hasOption("mongohost")) {
+            MongoConfiguration.HOST = command.getOptionValue("mongohost");
+        }
+
+        if(command.hasOption("mongodatabase")) {
+            MongoConfiguration.DATABASE_NAME = command.getOptionValue("mongodatabase");
+        }
+
         SpringApplication.run(Application.class, args);
+
     }
 
     @Override
---
+++ src/main/java/com/codecompiler/configuration/MongoConfiguration.java
@@ -0,0 +1,26 @@
+package com.codecompiler.configuration;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+/**
+ * Created by Manohar Prabhu on 5/30/2016.
+ */
+@Configuration
+public class MongoConfiguration extends AbstractMongoConfiguration {
+
+    public static String DATABASE_NAME = "codecompiler";
+    public static String HOST = "127.0.0.1";
+
+    @Override
+    protected String getDatabaseName() {
+        return DATABASE_NAME;
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient(HOST);
+    }
+}
