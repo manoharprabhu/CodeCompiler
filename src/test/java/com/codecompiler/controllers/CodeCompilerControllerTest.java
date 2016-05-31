4094bc4ac9d133d789cdde8a66d2adefabc4f85a
manoharprabhu <manoharagent13@gmail.com>
2016-05-31 20:20:15 +0530

    Added controller tests

---
+++ src/test/java/com/codecompiler/controllers/CodeCompilerControllerTest.java
@@ -0,0 +1,34 @@
+package com.codecompiler.controllers;
+
+import com.codecompiler.services.CodeCompilerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Created by Manohar Prabhu on 5/31/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class CodeCompilerControllerTest {
+
+    @Mock
+    private CodeCompilerService codeCompilerService;
+
+    @InjectMocks
+    private CodeCompilerController codeCompilerController = new CodeCompilerController();
+
+    @Test
+    public void testSubmitProgram() {
+        codeCompilerController.submitProgram("program", "input");
+        Mockito.verify(codeCompilerService, Mockito.times(1)).submitProgram("program", "input");
+    }
+
+    @Test
+    public void testCheckProgramStatus() {
+        codeCompilerController.checkProgramStatus("queueId");
+        Mockito.verify(codeCompilerService, Mockito.times(1)).checkProgramStatus("queueId");
+    }
+}
